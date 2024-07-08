337120c3ecd833f017fc05899255e0f060f58153
Lab <us23586@bristol.ac.uk>
2024-07-08 16:48:47 +0100

    reduce socket files

--- astro/src/main.rs
+++ astro/src/main.rs
@@ -3,12 +3,8 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, default_value_t = String::from("kinetics.sock"))]
-    kinetics_sock: String,
-    #[arg(short, long, default_value_t = String::from("gps.sock"))]
-    gps_sock: String,
-    #[arg(short, long, default_value_t = String::from("messages.sock"))]
-    messages_sock: String,
+    #[arg(short, long, default_value_t = String::from("socket"))]
+    socket_file: String,
 }
 
 fn main() {
