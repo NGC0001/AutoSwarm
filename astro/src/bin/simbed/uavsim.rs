fc6b424f721e33ec9f5056fc883f60938a6d25e6
Venice <25238465+NGC0001@users.noreply.github.com>
2024-07-15 00:11:40 +0100

    uavsim

--- astro/src/bin/simbed/uavsim.rs
+++ astro/src/bin/simbed/uavsim.rs
@@ -1,7 +1,39 @@
+use std::os::unix::net::UnixStream;
 use std::{cell::RefCell, rc::Rc};
+use std::time::{Duration, Instant};
 
+use astro::control::Velocity;
+use astro::gps::Position;
 use astro::transceiver::Transceiver;
 
+struct DataPack {
+    p: Position,
+    data_vec: Rc<Vec<String>>,
+}
+
 struct UavSim {
+    id: u32,
+    p: Position,
+    p_calc_t: Instant,
+    p_send_t: Instant,
+    v: Velocity,
+    out_msg: Vec<String>,  // messages from this UAV, to be sent to other ones
+    in_msg: Vec<String>,  // messages from other UAVs, to be received by this one
     tc: Rc<RefCell<Transceiver>>,
+}
+
+impl UavSim {
+    fn new(id: u32, p: Position, stream: UnixStream) -> UavSim {
+        let now = Instant::now();
+        UavSim {
+            id,
+            p,
+            p_calc_t: now,
+            p_send_t: now - Duration::from_secs(1000),
+            v: Velocity {vx: 0.0, vy: 0.0, vz: 0.0},
+            out_msg: vec![],
+            in_msg: vec![],
+            tc: Rc::new(RefCell::new(Transceiver::new(stream))),
+        }
+    }
 }
